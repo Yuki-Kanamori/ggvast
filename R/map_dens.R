16d9df85671324360715c38ac3645f8091fc8892
Yuki-Kanamori <y.kanamory@gmail.com>
2020-01-09 12:19:40 +0900

    added params

--- R/map_dens.R
+++ R/map_dens.R
@@ -1,23 +1,29 @@
-data = df_dens
-scale_name = "Log density"
-#labs = labs(title = "", x = "Longitude", y = "Latitude", colour = "Log density")
-ncol = 5 #number of figures in line side by side (max is no. of "Category")
-shape = 16 #16 is closed dot
-size = 1.9 #size of shape
-region = "Japan"
-#region_list = unique(map_data("world")$region)
-
-map_dens(data = df_dens,
-         scale_name = "Log density",
-         ncol = 5,
-         shape = 16,
-         size = 1.9,
-         region = "Japan")
-
-
-require(maps)
-require(mapdata)
-unique(world_map$region)
+#' Plot the COG on map from VAST output
+#'
+#' plot the COG om map using ggplot2. category names, axis names, font size and plot shape can change.
+#' @param data dataframe made by get_dens function
+#' @param region region of the data.
+#' @param scale_names unit of estimated density
+#' @param ncol number of figures in line side by side. max is no. of "Category"
+#' @param shape shape of COG point
+#' @param size size of shape
+#' @importForm magrittr %>%
+#' @importFrom dplyr filter
+#' @importFrom ggplot2 ggsave
+#' @import maps
+#' @import mapdata
+#' @import ggplot2
+#'
+#' @importFrom magrittr %>%
+#' @name %>%
+#' @rdname pipe
+#' @export
+#' @param lhs,rhs specify what lhs and rhs are
+#' @examples
+#' # some examples if you want to highlight the usage in the package
+NULL
+#'
+#' @export
 
 map_dens = function(data, region, scale_name, ncol, shape, size){
   map = ggplot() + coord_fixed() + xlab("Longitude") + ylab("Latitude")
@@ -35,14 +41,16 @@ map_dens = function(data, region, scale_name, ncol, shape, size){
              strip.text = element_text(size = rel(1.3)),
              legend.title = element_text(size = 13))
 
+  #single-species
   if(length(unique(data$category)) == 1){
     p = geom_point(data = data, aes(x = lon, y = lat, colour = log_abundance), shape = shape, size = size)
     f = facet_wrap( ~ year, ncol = ncol)
     c = scale_colour_gradientn(colours = c("black", "blue", "cyan", "green", "yellow", "orange", "red", "darkred"))
     fig = local_map+theme_bw()+th+p+f+c+labs(title = "", x = "Longitude", y = "Latitude", colour = scale_name)
-    ggsave(fig, )
+    #ggsave(fig, )
   }
 
+  #multi-species
   if(length(unique(data$category)) != 1){
     for(i in 1:length(data$category)){
       data2 = data %>% filter(category == 1)
@@ -50,7 +58,7 @@ map_dens = function(data, region, scale_name, ncol, shape, size){
       f = facet_wrap( ~ year, ncol = ncol)
       c = scale_colour_gradientn(colours = c("black", "blue", "cyan", "green", "yellow", "orange", "red", "darkred"))
       fig = local_map+theme_bw()+th+p+f+c+labs(title = paste(unique(data2$category_name)), x = "Longitude", y = "Latitude", colour = scale_name)
-      ggsave(fig, )
+      #ggsave(fig, )
     }
   }
 }
