624e230832d191061938dd1ec2740781d5492e4b
Yuki-Kanamori <y.kanamory@gmail.com>
2020-01-13 17:18:00 +0900

    bugs

--- pre_R/p_map_cog.R
+++ pre_R/p_map_cog.R
@@ -57,62 +57,64 @@ map_cog = function(data_type, category_name, region, ncol, shape, size, package,
                                              Year_Set = nyear_set,
                                              category_names = category_name)$COG_Table
     }
+    setwd(dir = vast_output_dirname)
+    write.csv(COG, "COG_Table.csv", fileEncoding = fileEncoding)
+
+    cog = read.csv("COG_Table.csv")
+
+    #UTM to longitude and latitude
+    #year_set = DG %>% select(Year) %>% distinct(Year, .keep_all = T)
+    tag = data.frame(rep(min(DG$Year):max(DG$Year), each = length(category_name)))
+    #tag = data.frame(rep(year_set, each = length(category_name)))
+    tag$Category = rep(category_name)
+    colnames(tag) = c("Year", "Category")
+
+    if(length(unique(category_name)) == 1){
+      cog = cbind(cog, Category = category_name)
+    }
+
+    head(cog)
+    head(tag)
+    cog = merge(cog, tag, by = c("Category", "Year"))
+
+    lat = cog[cog$m == 1, ]
+    lon = cog[cog$m == 2, ]
+    x = lat$COG_hat*1000
+    y = lon$COG_hat*1000
+    xy = cbind(x,y)
+    zone = unique(DG$zone)
+    lonlat = data.frame(project(xy, paste0("+proj=utm +zone=", zone, " ellps=WGS84"), inv = TRUE))
+    colnames(lonlat) = c("lon", "lat")
+    lonlat = cbind(lonlat, lat[, c("Year", "Category")])
+
+
+    #make COG maps
+    setwd(dir = map_output_dirname)
+    map = ggplot() + coord_fixed() + xlab("Longitude") + ylab("Latitude")
+    world_map = map_data("world")
+    region2 = subset(world_map, world_map$region == region)
+    local_map = map + geom_polygon(data = region2, aes(x = long, y = lat, group = group), colour = "black", fill = "white") + coord_map(xlim = c(min(lonlat$lon)-1, max(lonlat$lon)+1), ylim = c(min(lonlat$lat)-1, max(lonlat$lat)+1))
+    th = theme(panel.grid.major = element_blank(),
+               panel.grid.minor = element_blank(),
+               axis.text.x = element_text(size = rel(1.5)),
+               axis.text.y = element_text(size = rel(1.5)),
+               axis.title.x = element_text(size = rel(1.5)),
+               axis.title.y = element_text(size = rel(1.5)),
+               legend.title = element_text(size = 13))
+    p = geom_point(data = lonlat, aes(x = lon, y = lat, colour = Year), shape = shape, size = size)
+    f = facet_wrap( ~ Category, ncol = ncol)
+    c = scale_colour_gradientn(colours = c("black", "blue", "cyan", "green", "yellow", "orange", "red", "darkred"))
+    labs = labs(x = "Longitude", y = "Latitude", colour = "Year")
+
+    if(length(unique(category_name)) == 1){
+      fig = local_map+theme_bw()+th+p+c+labs
+    }else{
+      fig = local_map+theme_bw()+th+p+f+c+labs
+    }
+    ggsave(filename = "map_cog.pdf", plot = fig, units = "in", width = 11.69, height = 8.27)
   }
-  setwd(dir = vast_output_dirname)
-  write.csv(COG, "COG_Table.csv", fileEncoding = fileEncoding)
-
-  cog = read.csv("COG_Table.csv")
-
-  #UTM to longitude and latitude
-  #year_set = DG %>% select(Year) %>% distinct(Year, .keep_all = T)
-  tag = data.frame(rep(min(DG$Year):max(DG$Year), each = length(category_name)))
-  #tag = data.frame(rep(year_set, each = length(category_name)))
-  tag$Category = rep(category_name)
-  colnames(tag) = c("Year", "Category")
-
-  if(length(unique(category_name)) == 1){
-    cog = cbind(cog, Category = category_name)
-  }
-
-  head(cog)
-  head(tag)
-  cog = merge(cog, tag, by = c("Category", "Year"))
-
-  lat = cog[cog$m == 1, ]
-  lon = cog[cog$m == 2, ]
-  x = lat$COG_hat*1000
-  y = lon$COG_hat*1000
-  xy = cbind(x,y)
-  zone = unique(DG$zone)
-  lonlat = data.frame(project(xy, paste0("+proj=utm +zone=", zone, " ellps=WGS84"), inv = TRUE))
-  colnames(lonlat) = c("lon", "lat")
-  lonlat = cbind(lonlat, lat[, c("Year", "Category")])
 
 
-  #make COG maps
-  setwd(dir = map_output_dirname)
-  map = ggplot() + coord_fixed() + xlab("Longitude") + ylab("Latitude")
-  world_map = map_data("world")
-  region2 = subset(world_map, world_map$region == region)
-  local_map = map + geom_polygon(data = region2, aes(x = long, y = lat, group = group), colour = "black", fill = "white") + coord_map(xlim = c(min(lonlat$lon)-1, max(lonlat$lon)+1), ylim = c(min(lonlat$lat)-1, max(lonlat$lat)+1))
-  th = theme(panel.grid.major = element_blank(),
-             panel.grid.minor = element_blank(),
-             axis.text.x = element_text(size = rel(1.5)),
-             axis.text.y = element_text(size = rel(1.5)),
-             axis.title.x = element_text(size = rel(1.5)),
-             axis.title.y = element_text(size = rel(1.5)),
-             legend.title = element_text(size = 13))
-  p = geom_point(data = lonlat, aes(x = lon, y = lat, colour = Year), shape = shape, size = size)
-  f = facet_wrap( ~ Category, ncol = ncol)
-  c = scale_colour_gradientn(colours = c("black", "blue", "cyan", "green", "yellow", "orange", "red", "darkred"))
-  labs = labs(x = "Longitude", y = "Latitude", colour = "Year")
-
-  if(length(unique(category_name)) == 1){
-    fig = local_map+theme_bw()+th+p+c+labs
-  }else{
-    fig = local_map+theme_bw()+th+p+f+c+labs
-  }
-  ggsave(filename = "map_cog.pdf", plot = fig, units = "in", width = 11.69, height = 8.27)
 
 
   #nominal data
@@ -121,10 +123,10 @@ map_cog = function(data_type, category_name, region, ncol, shape, size, package,
     map = ggplot() + coord_fixed() + xlab("Longitude") + ylab("Latitude")
     world_map = map_data("world")
     region2 = subset(world_map, world_map$region == region)
-    local_map = map + geom_polygon(data = region2, aes(x = long, y = lat, group = group), colour = "gray 50", fill = "gray 50") + coord_map(xlim = c(min(cog_nom$lon)-1, max(cog_nom$lon)+1), ylim = c(min(cog_nom$lat)-1, max(cog_nom$lat)+1))
+    local_map = map + geom_polygon(data = region2, aes(x = long, y = lat, group = group), colour = "black", fill = "white") + coord_map(xlim = c(min(cog_nom$lon)-1, max(cog_nom$lon)+1), ylim = c(min(cog_nom$lat)-1, max(cog_nom$lat)+1))
     th = theme(panel.grid.major = element_blank(),
                panel.grid.minor = element_blank(),
-               axis.text.x = element_text(size = rel(1.5)),
+               axis.text.x = element_text(size = rel(1.5), angle = 90),
                axis.text.y = element_text(size = rel(1.5)),
                axis.title.x = element_text(size = rel(1.5)),
                axis.title.y = element_text(size = rel(1.5)),
@@ -134,7 +136,7 @@ map_cog = function(data_type, category_name, region, ncol, shape, size, package,
     c = scale_colour_gradientn(colours = c("black", "blue", "cyan", "green", "yellow", "orange", "red", "darkred"))
     labs = labs(x = "Longitude", y = "Latitude", colour = "Year")
 
-    if(length(unique(cog_nom$spp)) == 1){
+    if(!("spp" %in% names(cog_nom))){
       fig_nom =local_map+theme_bw()+th+p+c+labs
     }else{
       fig_nom = local_map+theme_bw()+th+p+f+c+labs
