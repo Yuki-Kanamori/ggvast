f09d30ed09e13c250862b46e4cc243228a66fe66
Yuki-Kanamori <y.kanamory@gmail.com>
2020-01-13 16:23:14 +0900

    revised get_cog

--- pre_R/p_get_cog.R
+++ pre_R/p_get_cog.R
@@ -5,23 +5,20 @@ require(dplyr)
 
 
 # please change here --------------------------------------------
-vast_output_dirname = "/Users/Yuki/Dropbox/iwac/iwac_MacPro/vast2019-07-19_lnorm_log100sardine"
+vast_output_dirname = "///"
 
 # load data -----------------------------------------------------
 setwd(dir = vast_output_dirname)
 DG = read.csv("Data_Geostat.csv")
 
-
 # make function -------------------------------
 # !!! DO NOT CHANGE HERE !!! ------------------------------------
 get_cog = function(data){
-  #no. of category
-  n_spp = length(unique(DG$spp))
 
   #single-species
-  if(n_spp == 1){
-    #cog = sum_{year}{Catch_KG*Lon}/sum_{year}{Catch_KG}を計算
-    dg = DG %>% select(Catch_KG, Lon, Lat) %>% mutate(cog_lon = Catch_KG*Lon, cog_lat = Catch_KG*Lat)
+  if(!("spp" %in% names(data))){
+    #cog_lon = sum_{year}{Catch_KG*Lon}/sum_{year}{Catch_KG}を計算
+    dg = DG %>% select(Catch_KG, Lon, Lat, Year) %>% mutate(cog_lon = Catch_KG*Lon, cog_lat = Catch_KG*Lat)
 
     lon_nume = ddply(dg, .(Year), summarize, nume = sum(cog_lon))
     lon_deno = ddply(dg, .(Year), summarize, deno = sum(Catch_KG))
@@ -31,13 +28,13 @@ get_cog = function(data){
     lat_deno = ddply(dg, .(Year), summarize, deno = sum(Catch_KG))
     cog_lat = merge(lat_nume, lat_deno, by = "Year") %>% mutate(lat = nume/deno) %>% select(Year, lat)
 
-    cog_nom = merge(cog_lon, cog_lat, by = "Year")
+    cog_nominal = merge(cog_lon, cog_lat, by = "Year")
   }
 
   #multi-species
-  if(n_spp != 1){
+  else{
     #cog = sum_{year}{Catch_KG*Lon}/sum_{year}{Catch_KG}を計算
-    dg = DG %>% select(Catch_KG, Lon, Lat) %>% mutate(cog_lon = Catch_KG*Lon, cog_lat = Catch_KG*Lat)
+    df = DG %>% select(Catch_KG, Lon, Lat, Year, spp) %>% mutate(cog_lon = Catch_KG*Lon, cog_lat = Catch_KG*Lat)
 
     lon_nume = ddply(df, .(Year, spp), summarize, nume = sum(cog_lon))
     lon_deno = ddply(df, .(Year, spp), summarize, deno = sum(Catch_KG))
@@ -45,9 +42,9 @@ get_cog = function(data){
 
     lat_nume = ddply(df, .(Year, spp), summarize, nume = sum(cog_lat))
     lat_deno = ddply(df, .(Year, spp), summarize, deno = sum(Catch_KG))
-    cog_lat = merge(lat_nume, lat_deno, by = c("Year", spp)) %>% mutate(lat = nume/deno) %>% select(Year, lat, spp)
+    cog_lat = merge(lat_nume, lat_deno, by = c("Year", "spp")) %>% mutate(lat = nume/deno) %>% select(Year, lat, spp)
 
-    cog_nom = merge(cog_lon, cog_lat, by = c("Year", "spp"))
+    cog_nominal = merge(cog_lon, cog_lat, by = c("Year", "spp"))
   }
 }
 
