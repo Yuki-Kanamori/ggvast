9e3c064cdf05b7b282d7854b497ff9f0308a203c
Yuki-Kanamori <y.kanamory@gmail.com>
2020-01-26 19:12:28 +0900

    changed

--- R/map_cog.R
+++ R/map_cog.R
@@ -1,93 +1,140 @@
 #' Plot the COG on map from VAST output
 #'
 #' plot the COG om map using ggplot2. category names, axis names, font size and plot shape can change.
+#' @param data_type VAST ot nomical
 #' @param category_name names of each category. set 1 when single-species model
-#' @param zone no. of UTM which was estimated when "Extrapolation_List" was made
-#' @param labs setting of figure. colour is legend
+#' @param region map region
 #' @param ncol number of figures in line side by side. max is no. of "Category"
 #' @param shape shape of COG point
 #' @param size size of shape
 #' @param package Spatial DeltaGLMM or FishStatsUtils
+#' @param map_output_dirname
+#' @param fileEncoding encoding of COG_Table.csv
 #' @importFrom SpatialDeltaGLMM Plot_range_shifts
 #' @importFrom FishStatsUtils plot_range_index
 #' @importFrom dplyr distinct
 #' @importFrom dplyr select
+#' @importFrom rgdal project
 #' @import magrittr
 #' @import maps
 #' @import mapdata
 #' @import ggplot2
+#' @import rgdal
 #'
 #' @export
 
 
-map_cog = function(category_name, region, labs, ncol, shape, size, package){
-  #make a COG_Table using VAST package
-  #re-make default figures of COG and Effective Area
-  if(package == "SpatialDeltaGLMM"){
-    require(SpatialDeltaGLMM)
-    cog = SpatialDeltaGLMM::Plot_range_shifts(Report = Save$Report,
-                                              TmbData = Save$TmbData,
-                                              Sdreport = Save$Opt$SD,
-                                              Znames = colnames(Save$TmbData$Z_xm),
-                                              PlotDir = DateFile,
-                                              use_biascorr = TRUE,
-                                              Year_Set = year_set,
-                                              category_names = category_name)$COG_Table
-  }else{
-    cog = FishStatsUtils::plot_range_index(Report = Save$Report,
-                                           TmbData = Save$TmbData,
-                                           Sdreport = Save$Opt$SD,
-                                           Znames = colnames(Save$TmbData$Z_xm),
-                                           PlotDir = DateFile,
-                                           use_biascorr = TRUE,
-                                           Year_Set = year_set,
-                                           category_names = category_name)$COG_Table
+map_cog = function(data_type, category_name, region, ncol, shape, size, package, map_output_dirname, fileEncoding){
+
+  setwd(dir = map_output_dirname)
+  nyear_set = seq(min(DG$Year), max(DG$Year))
+
+  #VAST data
+  if(data_type == "VAST"){
+    #get the COG information
+    if(package == "SpatialDeltaGLMM"){
+      COG = SpatialDeltaGLMM::Plot_range_shifts(Report = Save$Report,
+                                                TmbData = Save$TmbData,
+                                                Sdreport = Save$Opt$SD,
+                                                Znames = colnames(Save$TmbData$Z_xm),
+                                                PlotDir = vast_output_dirname,
+                                                use_biascorr = TRUE,
+                                                Year_Set = nyear_set,
+                                                category_names = category_name)$COG_Table
+    }else{
+      COG = FishStatsUtils::plot_range_index(Report = Save$Report,
+                                             TmbData = Save$TmbData,
+                                             Sdreport = Save$Opt$SD,
+                                             Znames = colnames(Save$TmbData$Z_xm),
+                                             PlotDir = vast_output_dirname,
+                                             use_biascorr = TRUE,
+                                             Year_Set = nyear_set,
+                                             category_names = category_name)$COG_Table
+    }
+    setwd(dir = vast_output_dirname)
+    write.csv(COG, "COG_Table.csv", fileEncoding = fileEncoding)
+
+    cog = read.csv("COG_Table.csv")
+
+    #UTM to longitude and latitude
+    #year_set = DG %>% select(Year) %>% distinct(Year, .keep_all = T)
+    tag = data.frame(rep(min(DG$Year):max(DG$Year), each = length(category_name)))
+    #tag = data.frame(rep(year_set, each = length(category_name)))
+    tag$Category = rep(category_name)
+    colnames(tag) = c("Year", "Category")
+
+    if(length(unique(category_name)) == 1){
+      cog = cbind(cog, Category = category_name)
+    }
+
+    head(cog)
+    head(tag)
+    cog = merge(cog, tag, by = c("Category", "Year"))
+
+    lat = cog[cog$m == 1, ]
+    lon = cog[cog$m == 2, ]
+    x = lat$COG_hat*1000
+    y = lon$COG_hat*1000
+    xy = cbind(x,y)
+    zone = unique(DG$zone)
+    lonlat = data.frame(project(xy, paste0("+proj=utm +zone=", zone, " ellps=WGS84"), inv = TRUE))
+    colnames(lonlat) = c("lon", "lat")
+    lonlat = cbind(lonlat, lat[, c("Year", "Category")])
+
+
+    #make COG maps
+    setwd(dir = map_output_dirname)
+    map = ggplot() + coord_fixed() + xlab("Longitude") + ylab("Latitude")
+    world_map = map_data("world")
+    region2 = subset(world_map, world_map$region == region)
+    local_map = map + geom_polygon(data = region2, aes(x = long, y = lat, group = group), colour = "black", fill = "white") + coord_map(xlim = c(min(lonlat$lon)-1, max(lonlat$lon)+1), ylim = c(min(lonlat$lat)-1, max(lonlat$lat)+1))
+    th = theme(panel.grid.major = element_blank(),
+               panel.grid.minor = element_blank(),
+               axis.text.x = element_text(size = rel(1.5)),
+               axis.text.y = element_text(size = rel(1.5)),
+               axis.title.x = element_text(size = rel(1.5)),
+               axis.title.y = element_text(size = rel(1.5)),
+               legend.title = element_text(size = 13))
+    p = geom_point(data = lonlat, aes(x = lon, y = lat, colour = Year), shape = shape, size = size)
+    f = facet_wrap( ~ Category, ncol = ncol)
+    c = scale_colour_gradientn(colours = c("black", "blue", "cyan", "green", "yellow", "orange", "red", "darkred"))
+    labs = labs(x = "Longitude", y = "Latitude", colour = "Year")
+
+    if(length(unique(category_name)) == 1){
+      fig = local_map+theme_bw()+th+p+c+labs
+    }else{
+      fig = local_map+theme_bw()+th+p+f+c+labs
+    }
+    ggsave(filename = "map_cog.pdf", plot = fig, units = "in", width = 11.69, height = 8.27)
   }
 
-  #from UTM to longitude and latitude
-  year_set = DG %>% select(Year) %>% distinct(Year, .keep_all = T)
-  tag = data.frame(rep(year_set, each = length(category_name)))
-  tag$Category = rep(category_name)
-  colnames(tag) = c("Year", "Category")
-  #tag$Category = factor(tag$Category, levels = category_name)
-  #levels(tag$Category)
-  if(category_name == 1){
-    cog$Category = 1
-  }
 
-  cog = merge(cog, tag, by = c("Category", "Year"))
 
-  lat = cog[cog$m == 1, ]
-  lon = cog[cog$m == 2, ]
-  x = lat$COG_hat*1000
-  y = lon$COG_hat*1000
-  xy = cbind(x,y)
-  zone = unique(DG$zone)
-  lonlat = data.frame(project(xy, paste0("+proj=utm +zone=", zone, " ellps=WGS84"), inv = TRUE))
-  colnames(lonlat) = c("lon", "lat")
-  lonlat = cbind(lonlat, lat[, c("Year", "Category")])
-  lonlat$Category = factor(lonlat$Category, levels = category_name)
 
-  #make COG maps
-  map = ggplot() + coord_fixed() + xlab("Longitude") + ylab("Latitude")
-  world_map = map_data("world")
-  region2 = subset(world_map, world_map$region == region)
-  local_map = map + geom_polygon(data = region2, aes(x = long, y = lat, group = group), colour = "gray 50", fill = "gray 50") + coord_map(xlim = c(min(data$lon)-1, max(data$lon)+1), ylim = c(min(data$lat)-1, max(data$lat)+1))
-  th = theme(panel.grid.major = element_blank(),
-             panel.grid.minor = element_blank(),
-             axis.text.x = element_text(size = rel(1.5)),
-             axis.text.y = element_text(size = rel(1.5)),
-             axis.title.x = element_text(size = rel(1.5)),
-             axis.title.y = element_text(size = rel(1.5)),
-             legend.title = element_text(size = 13))
-  p = geom_point(data = lonlat, aes(x = lon, y = lat, colour = Year), shape = shape, size = size)
-  f = facet_wrap( ~ Category, ncol = ncol)
-  c = scale_colour_gradientn(colours = c("black", "blue", "cyan", "green", "yellow", "orange", "red", "darkred"))
-  if(category_name == 1){
-    fig = local_map+theme_bw()+th+p+c+labs
-    ggsave(filename = "map_cog.pdf", plot = fig, units = "in", width = 8.27, height = 11.69)
-  }else{
-    fig = local_map+theme_bw()+th+p+f+c+labs
-    ggsave(filename = "map_cog.pdf", plot = fig, units = "in", width = 8.27, height = 11.69)
+  #nominal data
+  if(data_type == "nominal"){
+    #make COG maps
+    map = ggplot() + coord_fixed() + xlab("Longitude") + ylab("Latitude")
+    world_map = map_data("world")
+    region2 = subset(world_map, world_map$region == region)
+    local_map = map + geom_polygon(data = region2, aes(x = long, y = lat, group = group), colour = "black", fill = "white") + coord_map(xlim = c(min(cog_nom$lon)-1, max(cog_nom$lon)+1), ylim = c(min(cog_nom$lat)-1, max(cog_nom$lat)+1))
+    th = theme(panel.grid.major = element_blank(),
+               panel.grid.minor = element_blank(),
+               axis.text.x = element_text(size = rel(1.5), angle = 90),
+               axis.text.y = element_text(size = rel(1.5)),
+               axis.title.x = element_text(size = rel(1.5)),
+               axis.title.y = element_text(size = rel(1.5)),
+               legend.title = element_text(size = 13))
+    p = geom_point(data = cog_nom, aes(x = lon, y = lat, colour = Year), shape = shape, size = size)
+    f = facet_wrap( ~ spp, ncol = ncol)
+    c = scale_colour_gradientn(colours = c("black", "blue", "cyan", "green", "yellow", "orange", "red", "darkred"))
+    labs = labs(x = "Longitude", y = "Latitude", colour = "Year")
+
+    if(!("spp" %in% names(cog_nom))){
+      fig_nom =local_map+theme_bw()+th+p+c+labs
+    }else{
+      fig_nom = local_map+theme_bw()+th+p+f+c+labs
+    }
+    ggsave(filename = "map_cog_nominal.pdf", plot = fig_nom, units = "in", width = 8.27, height = 11.69)
   }
 }
--- R/map_dens.R
+++ R/map_dens.R
@@ -2,7 +2,6 @@
 #'
 #' plot the estimated density om map by species using ggplot2. scale name, plot shape, and size of plot shape can change.
 #' @param data dataframe made by get_dens function
-#' @param data_type VAST or nominal
 #' @param region region of the data
 #' \describe{
 #'   \item{[1] "Aruba", "Afghanistan", "Angola"}
@@ -94,26 +93,28 @@
 #' @param ncol number of figures in line side by side. max is no. of "Category"
 #' @param shape shape of COG point
 #' @param size size of shape
+#' @param map_output_dirname output directory
 #' @importFrom dplyr filter
 #' @importFrom ggplot2 ggsave
 #' @import maps
 #' @import mapdata
+#' @import dplyr
 #' @import ggplot2
 #' @import magrittr
 #'
 #' @export
+map_dens = function(data, region, scale_name, ncol, shape, size, map_output_dirname){
+  setwd(dir = map_output_dirname)
 
-map_dens = function(data, data_type, region, scale_name, ncol, shape, size){
-  if(data_type == "VAST"){
+  #plot the data from VAST
+  if("category_name" %in% names(data)){
     map = ggplot() + coord_fixed() + xlab("Longitude") + ylab("Latitude")
     world_map = map_data("world")
     region2 = subset(world_map, world_map$region == region)
-    local_map = map + geom_polygon(data = region2, aes(x = long, y = lat, group = group), colour = "gray 50", fill = "gray 50") + coord_map(xlim = c(min(data$lon)-1, max(data$lon)+1), ylim = c(min(data$lat)-1, max(data$lat)+1))
+    local_map = map + geom_polygon(data = region2, aes(x = long, y = lat, group = group), colour = "black", fill = "white") + coord_map(xlim = c(min(data$lon)-1, max(data$lon)+1), ylim = c(min(data$lat)-1, max(data$lat)+1))
     th = theme(panel.grid.major = element_blank(),
                panel.grid.minor = element_blank(),
-               #axis.text.x = element_text(size = rel(0.7), angle = 90),
                axis.text.x = element_blank(),
-               #axis.text.y = element_text(size = rel(0.7)),
                axis.text.y = element_blank(),
                axis.title.x = element_text(size = rel(1.5)),
                axis.title.y = element_text(size = rel(1.5)),
@@ -127,25 +128,26 @@ map_dens = function(data, data_type, region, scale_name, ncol, shape, size){
       c = scale_colour_gradientn(colours = c("black", "blue", "cyan", "green", "yellow", "orange", "red", "darkred"))
       fig = local_map+theme_bw()+th+p+f+c+labs(title = "", x = "Longitude", y = "Latitude", colour = scale_name)
       ggsave(filename = "map_dens.pdf", plot = fig, units = "in", width = 8.27, height = 11.69)
-    }
-
-    #multi-species
-    if(length(unique(data$category)) != 1){
-      for(i in 1:length(data$category)){
-        data2 = data %>% filter(category == 1)
+    }else{
+      #multi-species
+      for(i in 1:length(unique(data$category))){
+        data2 = data %>% filter(category == i)
         p = geom_point(data = data2, aes(x = lon, y = lat, colour = log_abundance), shape = shape, size = size)
         f = facet_wrap( ~ year, ncol = ncol)
         c = scale_colour_gradientn(colours = c("black", "blue", "cyan", "green", "yellow", "orange", "red", "darkred"))
-        fig = local_map+theme_bw()+th+p+f+c+labs(title = paste(unique(data2$category_name)), x = "Longitude", y = "Latitude", colour = scale_name)
-        ggsave(filename = "map_dens.pdf", plot = fig, units = "in", width = 8.27, height = 11.69)
+        fig = local_map+theme_bw()+th+p+f+c+labs(x = "Longitude", y = "Latitude", colour = scale_name)
+        ggsave(filename = paste0("map_dens_", unique(data2$category_name), ".pdf"), plot = fig, units = "in", width = 8.27, height = 11.69)
       }
     }
   }
-  if(data_type == "nominal"){
+
+
+  #plot the nominal data
+  if("Catch_KG" %in% names(data)){
     map = ggplot() + coord_fixed() + xlab("Longitude") + ylab("Latitude")
     world_map = map_data("world")
     region2 = subset(world_map, world_map$region == region)
-    local_map = map + geom_polygon(data = region2, aes(x = long, y = lat, group = group), colour = "gray 50", fill = "gray 50") + coord_map(xlim = c(min(data$lon)-1, max(data$lon)+1), ylim = c(min(data$lat)-1, max(data$lat)+1))
+    local_map = map + geom_polygon(data = region2, aes(x = long, y = lat, group = group), colour = "black", fill = "white") + coord_map(xlim = c(min(data$Lon)-1, max(data$Lon)+1), ylim = c(min(data$Lat)-1, max(data$Lat)+1))
     th = theme(panel.grid.major = element_blank(),
                panel.grid.minor = element_blank(),
                #axis.text.x = element_text(size = rel(0.7), angle = 90),
@@ -158,7 +160,7 @@ map_dens = function(data, data_type, region, scale_name, ncol, shape, size){
                legend.title = element_text(size = 13))
 
     #single-species
-    if(length(unique(data$category)) == 1){
+    if(!("spp" %in% names(data))){
       p = geom_point(data = data, aes(x = Lon, y = Lat, colour = Catch_KG), shape = shape, size = size)
       f = facet_wrap( ~ Year, ncol = ncol)
       c = scale_colour_gradientn(colours = c("black", "blue", "cyan", "green", "yellow", "orange", "red", "darkred"))
@@ -167,14 +169,15 @@ map_dens = function(data, data_type, region, scale_name, ncol, shape, size){
     }
 
     #multi-species
-    if(length(unique(data$category)) != 1){
-      for(i in 1:length(data$category)){
-        data2 = data %>% filter(category == i)
+    if("spp" %in% names(data)){
+      for(i in 1:length(unique(data$spp))){
+        data = data %>% mutate(nspp = as.numeric(spp))
+        data2 = data %>% filter(nspp == i)
         p = geom_point(data = data2, aes(x = Lon, y = Lat, colour = Catch_KG), shape = shape, size = size)
         f = facet_wrap( ~ Year, ncol = ncol)
         c = scale_colour_gradientn(colours = c("black", "blue", "cyan", "green", "yellow", "orange", "red", "darkred"))
-        fig = local_map+theme_bw()+th+p+f+c+labs(title = paste(unique(data2$category_name)), x = "Longitude", y = "Latitude", colour = scale_name)
-        ggsave(filename = paste0("map_dens_nominal_", category, ".pdf"), plot = fig, units = "in", width = 8.27, height = 11.69)
+        fig = local_map+theme_bw()+th+p+f+c+labs(title = "", x = "Longitude", y = "Latitude", colour = scale_name)
+        ggsave(filename = paste0("map_dens_nominal_", unique(data2$spp), ".pdf"), plot = fig, units = "in", width = 8.27, height = 11.69)
       }
     }
   }
