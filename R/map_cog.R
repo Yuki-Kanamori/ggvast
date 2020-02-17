96f2293b6273972908265c74759b8c93a62a1b5a
Yuki-Kanamori <y.kanamory@gmail.com>
2020-02-17 18:34:01 +0900

    add zoom option

--- R/map_cog.R
+++ R/map_cog.R
@@ -21,7 +21,7 @@
 #' @export
 
 
-map_cog = function(data_type, category_name, region, ncol, shape, size, fig_output_dirname){
+map_cog = function(data_type, category_name, region, ncol, shape, size, zoom, fig_output_dirname){
   if(data_type == "VAST"){
     # make COG Table
     ### this code is from plot_range_index() in FishStatsUtils ###
