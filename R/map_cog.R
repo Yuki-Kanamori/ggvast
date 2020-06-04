adf12b7eb7c6ab9f7aa941d07595f650b9323d44
Yuki-Kanamori <y.kanamory@gmail.com>
2020-06-04 16:13:17 +0900

    fixed bugs (zoon_in -> zoom_out)

--- R/map_cog.R
+++ R/map_cog.R
@@ -103,7 +103,7 @@ map_cog = function(data_type, category_name, region, ncol, shape, size, zoom_in_
     map = ggplot() + coord_fixed() + xlab("Longitude") + ylab("Latitude")
     world_map = map_data("world")
     region2 = subset(world_map, world_map$region == region)
-    local_map = map + geom_polygon(data = region2, aes(x = long, y = lat, group = group), colour = "black", fill = "white") + coord_map(xlim = c(min(data$lon)-1*zoom_in_lon*(-1), max(data$lon)+1*zoom_in_lon*(-1)), ylim = c(min(data$lat)-1*zoom_in_lat*(-1), max(data$lat)+1*zoom_in_lat*(-1)))
+    local_map = map + geom_polygon(data = region2, aes(x = long, y = lat, group = group), colour = "black", fill = "white") + coord_map(xlim = c(min(data$lon)-1*zoom_out_lon*(-1), max(data$lon)+1*zoom_out_lon*(-1)), ylim = c(min(data$lat)-1*zoom_out_lat*(-1), max(data$lat)+1*zoom_out_lat*(-1)))
     th = theme(panel.grid.major = element_blank(),
                panel.grid.minor = element_blank(),
                axis.text.x = element_text(size = rel(1.5)),
@@ -132,7 +132,7 @@ map_cog = function(data_type, category_name, region, ncol, shape, size, zoom_in_
     map = ggplot() + coord_fixed() + xlab("Longitude") + ylab("Latitude")
     world_map = map_data("world")
     region2 = subset(world_map, world_map$region == region)
-    local_map = map + geom_polygon(data = region2, aes(x = long, y = lat, group = group), colour = "black", fill = "white") + coord_map(xlim = c(min(data$lon)-1*zoom_in_lon*(-1), max(data$lon)+1*zoom_in_lon*(-1)), ylim = c(min(data$lat)-1*zoom_in_lat*(-1), max(data$lat)+1*zoom_in_lat*(-1)))
+    local_map = map + geom_polygon(data = region2, aes(x = long, y = lat, group = group), colour = "black", fill = "white") + coord_map(xlim = c(min(data$lon)-1*zoom_out_lon*(-1), max(data$lon)+1*zoom_out_lon*(-1)), ylim = c(min(data$lat)-1*zoom_out_lat*(-1), max(data$lat)+1*zoom_out_lat*(-1)))
     th = theme(panel.grid.major = element_blank(),
                panel.grid.minor = element_blank(),
                axis.text.x = element_text(size = rel(1.5)),
